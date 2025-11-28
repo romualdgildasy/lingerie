ea7993fd421accf502c90a199bb98d278eb655c8
Romuald <romualdgilads6@gmail.com>
2025-11-28 16:52:20 +0100

    installation des dependance et creation du modele de produit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        rose: {
+          50: '#fdf2f8',
+          600: '#ec4899',
+          900: '#500724'
+        }
+      }
+    },
+    plugins: [],
+  }
+}
